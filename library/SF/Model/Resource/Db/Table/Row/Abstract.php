49604eee5a7c202c66327301904a2580f3c8ddf6
muteor <thepope@thepopeisdead.com>
2009-02-06 13:33:45 +0000

    Adding comments to the row class

--- library/SF/Model/Resource/Db/Table/Row/Abstract.php
+++ library/SF/Model/Resource/Db/Table/Row/Abstract.php
@@ -11,13 +11,34 @@
  */
 abstract class SF_Model_Resource_Db_Table_Row_Abstract
 {
+    /**
+     * @var Zend_Db_Table_Row
+     */
     protected $_row = null;
 
+    /**
+     * Constructor.
+     *
+     * Supported params for $config are:-
+     * - rowClass = class name or object of type Zend_Db_Table_Abstract
+     *
+     * @param  array $config OPTIONAL Array of user-specified config options.
+     * @return void
+     */
     public function __construct(array $config = array()) 
     {
         $this->setRow($config);
     }
     
+    /**
+     * First looks for getter Methods for the columnName, that are used to lazy
+     * load dependant data (data not contained in the row).
+     * 
+     * Lastly proxies to the __get method of the row.
+     *
+     * @param string $columnName
+     * @return mixed
+     */
     public function __get($columnName)
     {
         $lazyLoader = 'get' . ucfirst($columnName);
@@ -28,21 +49,45 @@ abstract class SF_Model_Resource_Db_Table_Row_Abstract
         return $this->getRow()->__get($columnName);
     }
 
+    /**
+     * Checks if a row value is set, proxies to the __isset method
+     * of the row
+     *
+     * @param string $columnName
+     * @return boolean
+     */
     public function __isset($columnName)
     {
         return $this->getRow()->__isset($columnName);
     }
 
+    /**
+     * Proxies to the __set method on the row
+     *
+     * @param string $columnName
+     * @param mixed $value
+     * @return mixed
+     */
     public function __set($columnName, $value)
     {
         return $this->getRow()->__set($columnName, $value);
     }
     
+    /**
+     * Returns the connected row
+     *
+     * @return Zend_Db_Table_Abstract
+     */
     public function getRow()
     {
         return $this->_row;
     }
     
+    /**
+     * Sets the row
+     *
+     * @param array $config
+     */
     public function setRow(array $config = array())
     {
         $rowClass = 'Zend_Db_Table_Row';  
@@ -63,6 +108,13 @@ abstract class SF_Model_Resource_Db_Table_Row_Abstract
         throw new SF_Model_Exception('Could not set rowClass in ' . __CLASS__);
     }
     
+    /**
+     * Proxy method calls to the connected row, thing like toArray() etc
+     *
+     * @param string $method
+     * @param array $arguments
+     * @return mixed
+     */
     public function __call($method, array $arguments)
     {       
         return call_user_func_array(array($this->getRow(), $method), $arguments);
