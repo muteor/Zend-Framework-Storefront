d182812ad1959c38b9dced02b0b649c057439109
muteor <thepope@thepopeisdead.com>
2009-02-15 16:48:40 +0000

    ch6 updating productImage item

--- src/application/modules/storefront/models/resources/ProductImage/Item/Interface.php
+++ src/application/modules/storefront/models/resources/ProductImage/Item/Interface.php
@@ -9,7 +9,7 @@
  */
 interface Storefront_Resource_ProductImage_Item_Interface
 {
-    public function thumbnail();
-    public function full();
+    public function getThumbnail();
+    public function getFull();
     public function isDefault();
 }
