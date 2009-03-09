e262d6ff078ce8494ac078fb8b9f60aab7bf13a5
muteor <thepope@thepopeisdead.com>
2009-03-09 13:05:49 +0000

    Removed un-required code

--- application/modules/storefront/forms/EditUser.php
+++ application/modules/storefront/forms/EditUser.php
@@ -1,83 +1,23 @@
 <?php
 /**
- * Storefront_Form_Register
- *
- * The registration form
+ * Storefront_Form_EditUser
  *
  * @category   Storefront
  * @package    Storefront_Form
  * @copyright  Copyright (c) 2008 Keith Pope (http://www.thepopeisdead.com)
  * @license    http://www.thepopeisdead.com/license.txt     New BSD License
  */
-class Storefront_Form_EditUser extends Zend_Form
+class Storefront_Form_EditUser extends Storefront_Form_Register
 {
     public function init()
     {
-        // add path to custom validators
-        $this->addElementPrefixPath(
-            'Storefront_Validate',
-            APPLICATION_PATH . '/modules/storefront/models/validate/',
-            'validate'
-        );
+        //call the parent init
+        parent::init();
 
-        $this->addElement('select', 'title', array(
-            'required'   => true,
-            'label'      => 'Title',
-            'multiOptions' => array('Mr' => 'Mr','Ms' => 'Ms','Miss' => 'Miss','Mrs' => 'Mrs')
-        ));
-
-        $this->addElement('text', 'firstname', array(
-            'filters'    => array('StringTrim'),
-            'validators' => array(
-                'Alpha',
-                array('StringLength', true, array(3, 128))
-            ),
-            'required'   => true,
-            'label'      => 'Firstname',
-        ));
-
-        $this->addElement('text', 'lastname', array(
-            'filters'    => array('StringTrim'),
-            'validators' => array(
-                'Alpha',
-                array('StringLength', true, array(3, 128))
-            ),
-            'required'   => true,
-            'label'      => 'lastname',
-        ));
-
-        $this->addElement('text', 'email', array(
-            'filters'    => array('StringTrim', 'StringToLower'),
-            'validators' => array(
-                array('StringLength', true, array(3, 128)),
-                array('EmailAddress'),
-            ),
-            'required'   => true,
-            'label'      => 'Email',
-        ));
-
-        $this->addElement('password', 'passwd', array(
-            'filters'    => array('StringTrim'),
-            'validators' => array(
-                array('StringLength', true, array(6, 128))
-            ),
-            'required'   => false,
-            'label'      => 'Password',
-        ));
-
-        $this->addElement('password', 'passwdVerify', array(
-            'filters'    => array('StringTrim'),
-            'validators' => array(
-               'PasswordVerification',
-            ),
-            'required'   => false,
-            'label'      => 'Confirm Password',
-        ));
-
-        $this->addElement('submit', 'save', array(
-            'required' => false,
-            'ignore'   => true,
-            'label'    => 'Register',
-        ));
+        //customize the form
+        $this->getElement('email')->removeValidator('UniqueEmail');
+        $this->getElement('passwd')->setRequired(false);
+        $this->getElement('passwdVerify')->setRequired(false);
+        $this->getElement('register')->setLabel('Save User');
     }
 }
