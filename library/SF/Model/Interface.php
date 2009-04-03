604a49d2bd324b7869a0f9ba923a19d252464f70
muteor <thepope@thepopeisdead.com>
2009-04-03 10:03:51 +0000

    Updating the SF_Model_Interface

--- library/SF/Model/Interface.php
+++ library/SF/Model/Interface.php
@@ -11,6 +11,7 @@
  */
 interface SF_Model_Interface
 {
+    public function __construct($options = null);
     public function getResource($name);
     public function getForm($name);
     public function init();
