3f5fef4b3a85b2a1cf7074b4e5cba76f66460137
muteor <thepope@thepopeisdead.com>
2009-05-09 07:49:53 +0000

    changes for ch10

--- 010_Admin/application/modules/storefront/models/Catalog.php
+++ 010_Admin/application/modules/storefront/models/Catalog.php
@@ -7,7 +7,7 @@
  * @copyright  Copyright (c) 2008 Keith Pope (http://www.thepopeisdead.com)
  * @license    http://www.thepopeisdead.com/license.txt     New BSD License
  */
-class Storefront_Model_Catalog extends SF_Model_Acl_Abstract implements Zend_Acl_Resource_Interface
+class Storefront_Model_Catalog extends SF_Model_Acl_Abstract
 {    
     /**
      * Get categories
