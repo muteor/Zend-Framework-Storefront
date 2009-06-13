e659d4423d300c62ce3379adfc68e58a4ac611e3
muteor <thepope@thepopeisdead.com>
2009-06-13 09:03:30 +0000

    Branching ch12

---
+++ 012_Optimization/application/modules/storefront/models/resources/Productimage/Interface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Storefront_Resource_ProductImage_Interface
+ * 
+ * @category   Storefront
+ * @package    Storefront_Model_Resource
+ * @copyright  Copyright (c) 2008 Keith Pope (http://www.thepopeisdead.com)
+ * @license    http://www.thepopeisdead.com/license.txt     New BSD License
+ */
+interface Storefront_Resource_Productimage_Interface
+{
+    public function setDefault($image, $product);
+}
